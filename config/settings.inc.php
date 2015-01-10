65d96043c1394ad417106e70e0c694ce79521f80
Aleksandr Drobot <a.drobot@codetiburon.com>
2015-01-10 16:09:51 +0200

    first commit

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '221976');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'RgKdin7n290j5S6e8r2ESXJ0NLoRNTg4MTcPuwGg2xNKnNCGVXEqtAko');
+define('_COOKIE_IV_', 'xhpFKEKv');
+define('_PS_CREATION_DATE_', '2015-01-10');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.0.11');
+define('_RIJNDAEL_KEY_', 'TUXpoLjNY5FOUv3FAxXEeVz321cjot6Y');
+define('_RIJNDAEL_IV_', 'yOcI084PK7k1OF4zEmzzmQ==');
